592448bfef43e237e0a5c918763c903c66e4ef42
Matthew Podwysocki <matthewp@microsoft.com>
2018-10-24 13:16:20 -0700

    fix(appdelegate): use iOS 10 APIs

--- iOS/Samples/nhubsample/nhubsample/AppDelegate.m
+++ iOS/Samples/nhubsample/nhubsample/AppDelegate.m
@@ -16,16 +16,36 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    UNAuthorizationOptions options =  UNAuthorizationOptionAlert
+    | UNAuthorizationOptionSound
+    | UNAuthorizationOptionBadge;
     
-    UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeSound |
-                                            UIUserNotificationTypeAlert | UIUserNotificationTypeBadge categories:nil];
-    
-    [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    [[UNUserNotificationCenter currentNotificationCenter] setDelegate:self];
+    [center requestAuthorizationWithOptions:(options) completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        if (error != nil) {
+            NSLog(@"Error requesting for authorization: %@", error);
+        }
+    }];
     [[UIApplication sharedApplication] registerForRemoteNotifications];
     
     return YES;
 }
 
+-(void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler {
+    [self logAlert:notification.request.content.userInfo];
+    completionHandler(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge);
+}
+
+-(void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void(^)(void))completionHandler {
+    [self logAlert:response.notification.request.content.userInfo];
+    completionHandler();
+}
+
+-(void)logAlert:(NSDictionary *)userInfo {
+    NSLog(@"User Info : %@", userInfo);
+    [self MessageBox:@"Notification" message:[[userInfo objectForKey:@"aps"] valueForKey:@"alert"]];
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -54,30 +74,22 @@
 }
 
 - (void) application:(UIApplication *) application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *) deviceToken {
-    SBNotificationHub* hub = [[SBNotificationHub alloc] initWithConnectionString:HUBLISTENACCESS
-                                                             notificationHubPath:HUBNAME];
+    SBNotificationHub* hub = [[SBNotificationHub alloc] initWithConnectionString:HUBLISTENACCESS notificationHubPath:HUBNAME];
     
     [hub registerNativeWithDeviceToken:deviceToken tags:nil completion:^(NSError* error) {
         if (error != nil) {
             NSLog(@"Error registering for notifications: %@", error);
-        }
-        else {
+        } else {
             [self MessageBox:@"Registration Status" message:@"Registered"];
         }
     }];
 }
 
--(void)MessageBox:(NSString *) title message:(NSString *)messageText
-{
+-(void)MessageBox:(NSString *) title message:(NSString *)messageText {
     UIAlertController *alert = [UIAlertController alertControllerWithTitle:title message:messageText preferredStyle:UIAlertControllerStyleAlert];
     UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
     [alert addAction:okAction];
     [[[[UIApplication sharedApplication] keyWindow] rootViewController] presentViewController:alert animated:YES completion:nil];
-}
-
-- (void)application:(UIApplication *)application didReceiveRemoteNotification: (NSDictionary *)userInfo {
-    NSLog(@"%@", userInfo);
-    [self MessageBox:@"Notification" message:[[userInfo objectForKey:@"aps"] valueForKey:@"alert"]];
 }
 
 @end
