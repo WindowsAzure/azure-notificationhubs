dab58b2b76d5328054875b3a7d9ccd92f0c0832c
brannon <brannon@iterator.net>
2018-10-24 17:18:19 -0700

    Add AppCenter pre-build script.

---
+++ iOS/Samples/nhubsample/appcenter-pre-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+INFO_PLIST=$APPCENTER_SOURCE_DIRECTORY/nhubsample/Info.plist
+
+if [ "$NHUB_NAME" != "" ]; then
+    plutil -replace NotificationHubName -string "$NHUB_NAME" $INFO_PLIST
+fi
+
+if [ "$NHUB_CONNECTIONSTRING" != "" ]; then
+    plutil -replace NotificationHubConnectionString -string "$NHUB_CONNECTIONSTRING" $INFO_PLIST
+fi
